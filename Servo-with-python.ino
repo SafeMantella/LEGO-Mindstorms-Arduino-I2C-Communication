4572eba069620ce7fd726074d7b5f4254b16c946
Pedro Arfux <42593007+SafeMantella@users.noreply.github.com>
2021-12-08 21:42:24 -0400

    Create Servo-with-python.ino

---
+++ Servo-with-python.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include <Servo.h>
+ 
+#define SLAVE_ADDRESS 0x04
+#define SERVO_PIN        9
+ 
+Servo srv;
+ 
+void setup()
+{
+    Serial.begin(9600);         // start serial for output
+    Wire.begin(SLAVE_ADDRESS);
+    Wire.onReceive(receiveData);
+    srv.attach(SERVO_PIN);
+}
+
+int val,flag=0;//[EXPERIMENTO] 
+void loop()
+{
+  if(flag==1)
+   {
+     Serial.print(val);// val é o valor recebido pelo arduino, o valor não é alterado em seu processamento
+     flag=0;
+   }//[EXPERIMENTO]
+}
+ 
+void receiveData(int byteCount)
+{
+    while(Wire.available()>0)
+    {
+      val=Wire.read();//[EXPERIMENTO], se der errado, substitui val por Wire.read 
+      flag=1;//[EXPERIMENTO] 
+      
+      srv.write(val);//gira servo
+      Serial.print(val);//printa graus no monitor serial
+    }
+}
